046f3911b5c9319330b30fff5f649ebde4e07bc8
Madhu Dadi <madhu.kumar245@gmail.com>
2026-03-28 18:49:56 +0530

    initialise postgres db

---
+++ backend/app/core/db.py
@@ -0,0 +1,31 @@
+from typing import AsyncGenerator
+from .config import settings
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from .logging import get_logger
+
+
+logger = get_logger()
+
+engine = create_async_engine(settings.DATABASE_URL)
+
+async_session = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        try:
+            yield session
+        except Exception as e:
+            logger.error(f"An error occurred while getting the database session: {e}")
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+
+
+async def init_db() -> None:
+    pass
